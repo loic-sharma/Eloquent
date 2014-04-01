72e5eddacb33aa2a6b102c406455715c7053e863
Loic Sharma <sharma.loic@gmail.com>
2014-03-31 21:59:04 -0400

    Added header guard to the AST header file.

--- ast.h
+++ ast.h
@@ -1,3 +1,6 @@
+#ifndef __Eloquent_ast__
+#define __Eloquent_ast__
+
 class Node {
 public:
 	Node(const char *v, Node *l = nullptr, Node *r = nullptr) : value(v), left(l), right(r) {}
@@ -11,3 +14,5 @@ class CompoundNode : public Node {
 public:
 	CompoundNode(Node *l, Node *r) : Node("__compound", l, r) {}
 };
+
+#endif
